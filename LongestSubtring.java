0b66a848efbb715009d70f0ce18b2c373a041706
harry.wang <harry.wang@generalbiologic.com>
2016-04-26 16:46:28 +0800

    first upload

---
+++ LongestSubtring.java
@@ -0,0 +1,35 @@
+ public int GetLongestSubString(string inputStr)
+        {
+            char[] charArray = inputStr.ToArray();
+            Hashtable ht = new Hashtable();
+            int maxLength = 0;
+            int len = 0;
+            char tempChar;
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                tempChar = charArray[i];
+                if (!ht.ContainsKey(tempChar))
+                {
+                    ht.Add(tempChar,i);
+                    len++;
+                }            
+                else
+                {
+                    maxLength = GetLarger(maxLength, len);
+                    for (int j = 0; j < (int)ht[tempChar]; j++)
+                    {
+                        ht.Remove(charArray[j]);
+                        len = len - (int)ht[tempChar];
+                    }
+                }
+            }
+                return maxLength;
+        }
+
+        public int GetLarger(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
