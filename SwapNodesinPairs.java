73fa9a8155394f8c790445cb49a11c8718ea6661
harry.wang <harry.wang@generalbiologic.com>
2016-04-28 17:30:28 +0800

    Leetcode Solution -- Swap Nodes in Pairs

---
+++ SwapNodesinPairs.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static ListNode ReverseTwoNodes(ListNode head)
+    {
+        ListNode temp = head.next;
+        head.next = null;
+        temp.next = head;
+        return temp;
+    }
+
+
+    public ListNode swapPairs(ListNode head) 
+    { 
+        ListNode TheHead = head;
+        Do 
+        {
+            head = ReverseTwoNodes(head);
+            head = head.next.next;
+        }
+        while(head != null && head.next!= null) 
+    }
+    return TheHead;
+}
