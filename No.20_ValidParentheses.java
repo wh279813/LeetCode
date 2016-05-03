bcc99002dda57e71d895728dc12c623de7673569
harry.wang <harry.wang@generalbiologic.com>
2016-05-03 16:02:57 +0800

    No.20 Valid Parentheses

---
+++ No.20_ValidParentheses.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author harry.wang
+ */
+public class Solution {
+    public static boolean isValid(String s) {
+        Stack stack = new Stack();
+        char[] arr = s.toCharArray();
+        for(int i = 0; i < arr.length;i++)
+        {
+            if(arr[i]== '(' ||arr[i]== '{'||arr[i]== '[' )
+            {
+                stack.push(arr[0]);
+            }
+            else if(arr[i]== ')' ||arr[i]== '}'||arr[i]== ']' )
+            {
+                if((arr[i]-1) != (Character)stack.pop())
+                    return false;
+            }
+        }
+        if(!stack.isEmpty())
+            return false;
+        else
+            return true;
+    }
+}
