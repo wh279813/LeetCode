660ccf659390f5e64eae69f680c7c65417a8e2c1
harry.wang <harry.wang@generalbiologic.com>
2016-05-09 16:42:04 +0800

    No 27 remove element

---
+++ No.27_RemoveElement.java
@@ -0,0 +1,16 @@
+/* 虽然这个题只要求返回int值
+*  但是只遍历一下并返回一个累加的size得到的结果是wrong anwser.
+*  AC方法为：把不等于val的值放到原数组中，从0开始
+*/
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+    	int size = 0;
+    	for (int i=0;i<nums.length;i++) {
+    		if (nums[i] != val)
+    		nums[size++] = nums[i];
+    		
+    	}
+        return size;
+    }
+}
