2d02c69b68a64ac0d84ed04e116d3d1c667adaf1
harry.wang <harry.wang@generalbiologic.com>
2016-05-11 11:12:22 +0800

    three sum with brute-force

--- No.15_3Sum.java
+++ No.15_3Sum.java
@@ -6,14 +6,47 @@
  */
 package test;
 
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 /**
  *
  * @author harry.wang
  */
 
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
+public class Solution 
+ {
+ 	//Search by Brute-force algorithm---Time complexity O(N^3)  Too much!!!
+     public static List<List<Integer>> threeSumByEnum(int[] nums)
+    {
+        boolean Exitflag = false;
+     	List<List<Integer>> result = new ArrayList<List<Integer>>();
+     	for(int first=0;first<nums.length;first++)
+     	{
+     		for (int second=first+1;second<nums.length;second++ )
+     		{
+     			for (int third=second+1;third<nums.length;third++ ) 
+     			{
+     				if(nums[first] + nums[second] + nums[third] == 0)		//三次循环
+     				{
+     					List<Integer> SumToZero = new ArrayList<Integer>();
+                                         SumToZero.add(nums[first]);
+                                         SumToZero.add(nums[second]);
+                                         SumToZero.add(nums[third]);
+                                         Collections.sort(SumToZero);				//排序
+                                         for(int i =0;i<result.size();i++)		
+                                         {
+                                             if (result.get(i).equals(SumToZero))    //如果result没有这个SumToZero，才添加
+                                                 Exitflag = true;
+                                                                                                                                              
+                                         }
+                                         if(!Exitflag)
+                                         result.add(SumToZero); 
+                                         Exitflag = false;                                     
+     				}   				
+     			}
+     		}
+     	}     
+        System.out.println(result);  
+     	return result;
     }
-}
