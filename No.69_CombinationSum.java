05c4389001cf592c39d5737860116c168807a92c
HuAn <410305495@qq.com>
2016-06-26 14:50:44 +0800

    No 39 combination sum

--- No.69_CombinationSum.java
+++ No.69_CombinationSum.java
@@ -1,32 +1,48 @@
-/*
+/* 递归解决该问题：
+*   SearchMatch方法，不断迭代candidates,存储的candidates放入temp中
+*   直到target的值结束，超出target则退出
 */
 package javapractice;
 
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 /**
  *
  * @author wanghuan
  */
 
 public class myMethod {
-    public static int mySqrt(int x) {
-    	int max = x;
-    	int min = 0;
-    	int mid;
-        if(x == 1)
-            return 1;
-    	while(max - min > 1)
-    	{
-    		mid = min + (max - min)/2;
-            if(mid * mid > x)
-                max = mid;
-            else
-                min =  mid;                
-    	}
-
-        return min;
+    public  List<List<Integer>> combinationSum(int[] candidates, int target) 
+    {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        ArrayList<Integer> temp = new ArrayList<Integer>();     //Temp list, used to store current candidate member.
+        Arrays.sort(candidates);
+        SearchMatch(0,candidates,target,temp,result);
+        return result; 
     }
     
+    
+    public static void SearchMatch(int start,int[] candidates,int target,List<Integer> temp,List<List<Integer>> result)
+    {
+        for(int i = start; i < candidates.length; i ++)
+        {
+            if(target == candidates[i])
+            {
+                temp.add(candidates[i]);
+                result.add(new ArrayList<Integer>(temp));
+                temp.remove(temp.size() -1);   // Remove the last added element in temp.
+            }
+            else if(target > candidates[i])
+            {
+                temp.add(candidates[i]);
+                SearchMatch(i, candidates, target - candidates[i], temp, result);  //Recur to search the rest result members.
+                temp.remove(temp.size()-1);            
+            }
+            else
+                return;        
+        }
+    }
 }
 
 
