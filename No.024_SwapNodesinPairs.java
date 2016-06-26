656f6dff04a6ac66813f753c86cf4aa4b53f50d7
HuAn <410305495@qq.com>
2016-06-26 20:08:57 +0800

    change names

---
+++ No.024_SwapNodesinPairs.java
@@ -0,0 +1,27 @@
+//未完成
+public class Solution {
+    public static ListNode[] ReverseFirstTwo(ListNode head)
+    {
+        ListNode[] newSnake = new ListNode[2];
+        newSnake[1] = head.next.next;
+        ListNode Temp = head.next;
+        head.next = null;
+        Temp.next = head;
+        newSnake[0] = Temp;
+        return newSnake;
+    }
+
+
+    public ListNode swapPairs(ListNode head) 
+    { 
+        ListNode TheHead = head;
+        Do 
+        {
+            head = ReverseFirstTwo(head);
+            head = head.next.next;
+        }
+        while(head != null && head.next!= null) 
+        return TheHead;
+    }
+    
+}
