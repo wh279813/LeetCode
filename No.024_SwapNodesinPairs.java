2ef488c465938336d0bd9edbc07fbd2799f6f881
HuAn <410305495@qq.com>
2016-07-19 15:13:15 +0800

    no 24 swap nodes in paris

--- No.024_SwapNodesinPairs.java
+++ No.024_SwapNodesinPairs.java
@@ -1,27 +1,38 @@
-//未完成
+/* 链表每两位调换位置问题(按照普通思路曾经浪费了很长时间思考),
+* 问题的思路在于从后往前解决，借助递归实现,
+* 思路在很大程度上是和dfs相同的:
+*   1. 先走到链表的最尾端，返回空或者该节点
+*   2. 然后对于途中的一般情况，把前一个节点的next设为后面的递归结果，第二个节点的next设为第一个节点
+*   3. 递归函数的返回为第二个节点(用tmp表示)  
+*/
+package javapractice;
+
+
+
+
+/**
+ *
+ * @author wanghuan
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-    public static ListNode[] ReverseFirstTwo(ListNode head)
-    {
-        ListNode[] newSnake = new ListNode[2];
-        newSnake[1] = head.next.next;
-        ListNode Temp = head.next;
-        head.next = null;
-        Temp.next = head;
-        newSnake[0] = Temp;
-        return newSnake;
+    public ListNode swapPairs(ListNode head) {
+        if(head == null)
+            return null;
+        if(head.next == null)
+            return head;
+        // swap nodes
+        ListNode tmp = head.next;
+        head.next = swapPairs(head.next.next);
+        tmp.next = head;
+        return tmp;
     }
-
-
-    public ListNode swapPairs(ListNode head) 
-    { 
-        ListNode TheHead = head;
-        Do 
-        {
-            head = ReverseFirstTwo(head);
-            head = head.next.next;
-        }
-        while(head != null && head.next!= null) 
-        return TheHead;
-    }
-    
 }
