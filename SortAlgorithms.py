8d568c5e6aa6d6e8fef041d1fed6a9dd9e174ae3
HuAn <410305495@qq.com>
2016-08-30 22:43:08 +0800

    heap sort in python

--- SortAlgorithms.py
+++ SortAlgorithms.py
@@ -44,6 +44,33 @@ def quick_sort(list, left, right):
 	return list
 
 
+def sink(a, k, n): #自顶向下堆化，从k开始堆化 
+	N = n - 1;
+	while 2 * k <= N:
+		j = 2 * k
+		if j < N and a[j] < a[j + 1]: # 选出左右节点较大的一个
+			j += 1 
+		if a[k] < a[j]:
+			a[k], a[j] = a[j], a[k]
+			k = j
+		else:
+			break
+
+def heap_sort(list):
+	n = len(list) - 1
+	for i in range(n//2, 0 , -1):
+		sink(list, i, len(list))   # sink前n/2个实现一个大顶堆
+
+	while n > 1:
+		list[1], list[n] = list[n], list[1] # 依次把最大的元素放入最后
+		sink(list, 1, n)    # 重新下沉，构建大顶堆
+		n -= 1
+	return list
+
+
+
+
+
 
 test = [1, 3, 4, 5, 2]
-print(quick_sort(test, 0 ,4))
+print(heap_sort(test))
